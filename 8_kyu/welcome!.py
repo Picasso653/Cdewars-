3da3814ff763bd5fa8b563cf848fc1437bbd580e
Isaac Korankye Asante <xquisit31@gmail.com>
2022-11-06 17:38:35 +0000

    codewars solution

---
+++ 8_kyu/welcome!.py
@@ -0,0 +1,23 @@
+def greet(language):
+    lang_dict = {'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+}
+    if language in lang_dict:
+        return lang_dict[language]
+    else:
+        return 'Welcome'
