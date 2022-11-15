9b03efc4bc55a3b7224fce43e58a375b0251e717
Isaac Korankye Asante <xquisit31@gmail.com>
2022-11-15 14:22:53 +0000

    codewars challenge today

---
+++ 8_kyu/how_many_lightsabers_do_you_own.py
@@ -0,0 +1,8 @@
+def how_many_light_sabers_do_you_own(name = None):
+        if name == None:
+            return 0
+        if name == "Zach":
+            return 18
+        else:
+            return 0
+        return 0
