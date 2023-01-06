dc3bc5029357bde3141b37793253525a7931428e
Isaac Korankye Asante <xquisit31@gmail.com>
2023-01-06 16:17:18 +0000

    Daily codewars challlenge

--- 6_kyu/decode_the_morse_code.py
+++
@@ -1,6 +0,0 @@
-from preloaded import MORSE_CODE
-
-
-def decodeMorse(morse_code):
-    return ' '.join(''.join(MORSE_CODE[i] for i in word.split(' ')) for word in morse_code.strip().split("   "))
-   
---
+++ 6_kyu/is_a_number_prime?.py
@@ -0,0 +1,27 @@
+from random import randrange
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    r = 0
+    d = n - 1
+    while d % 2 == 0:
+        r += 1
+        d //= 2
+    for _ in range(10):
+        a = randrange(2, n - 1)
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
