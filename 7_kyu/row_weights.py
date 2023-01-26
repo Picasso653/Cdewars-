223de4dbad95a1d4642fcbb201401569f9f2204d
Isaac Korankye Asante <xquisit31@gmail.com>
2023-01-26 01:42:14 +0000

    correct one

--- 7_kyu/row_weights.py
+++ 7_kyu/row_weights.py
@@ -4,10 +4,4 @@ def row_weights(array):
     for i in range(len(array)):
         if i%2!=0:  team1.append(array[i])
         else:   team2.append(array[i])
-    return (sum(team2),sum(team1))def row_weights(array):
-    team1 = []
-    team2 = []
-    for i in range(len(array)):
-        if i%2!=0:  team1.append(array[i])
-        else:   team2.append(array[i])
     return (sum(team2),sum(team1))
